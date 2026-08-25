d959d8a3362cd9ccda00c2b99d8b1c1ea037a4ca
uglydog <22667051.thien@student.iuh.edu.vn>
2026-08-25 17:20:36 +0700

    feat: externalize WebSocket allowed origins to application configuration

--- src/main/java/com/lms/common/config/WebSocketConfig.java
+++ src/main/java/com/lms/common/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.lms.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.List;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -21,10 +23,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${lms.cors.allowed-origins:http://localhost:3000}")
+    private List<String> allowedOrigins;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:3000")
+                .setAllowedOriginPatterns(allowedOrigins.toArray(new String[0]))
                 .withSockJS();
     }
 
