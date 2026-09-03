5bc9b716fcc0b2847a9135ace010f4e7ef7b7b45
= <=>
2026-09-03 06:25:51 +0000

    fix: HQL query in AiUsageLogRepository resolving attribute user

--- src/main/java/com/lms/common/repository/AiUsageLogRepository.java
+++ src/main/java/com/lms/common/repository/AiUsageLogRepository.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Repository
 public interface AiUsageLogRepository extends JpaRepository<AiUsageLog, Long> {
     
-    @Query("SELECT l.user.id as userId, l.user.email as email, l.user.isAiLocked as isAiLocked, SUM(l.totalTokens) as totalTokens, SUM(l.costUsd) as totalCost " +
-           "FROM AiUsageLog l GROUP BY l.user.id, l.user.email, l.user.isAiLocked ORDER BY totalCost DESC")
+    @Query("SELECT u.id as userId, u.email as email, u.isAiLocked as isAiLocked, SUM(l.totalTokens) as totalTokens, SUM(l.costUsd) as totalCost " +
+           "FROM AiUsageLog l JOIN User u ON l.userId = u.id GROUP BY u.id, u.email, u.isAiLocked ORDER BY totalCost DESC")
     List<Map<String, Object>> getUsageSummaryByUser();
 }
